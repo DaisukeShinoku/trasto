69e0664f8185a1f39d6a98742fa0f76043bcff84
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-06-24 17:22:16 +0900

    [Add]Gems&Uploader

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,3 @@
 class House < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ApplicationRecord
   belongs_to :user
   belongs_to :house
+  mount_uploader :avatar, AvatarUploader
 end
