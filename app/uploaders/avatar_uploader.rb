db03b98266e5f5a0621a4c824cb0521f29566f74
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-06-24 20:09:29 +0900

    [Add]User-Login-Function(Basic)

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,22 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_limit: [900, 600]
+
+  version :small do
+    process resize_to_fit: [90, 60]
+  end
+
+  version :medium do
+    process resize_to_fit: [300, 200]
+  end
+
+  version :large do
+    process resize_to_fit: [900, 600]
+  end
+
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -35,9 +50,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
