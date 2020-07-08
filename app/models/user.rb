79d9757a04eb18cf6516c980d982c535e2b006b2
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-07-08 17:25:32 +0900

    signup-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   VALID_ACCOUNT_REGEX = /\A[0-9a-z_]{1,15}\z/i
   validates :account_name, presence: true, length: {maximum: 15}, format: { with: VALID_ACCOUNT_REGEX }, uniqueness: true
-  validates :display_name, presence: true
+  validates :display_name, presence: true, length: {maximum: 15}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
