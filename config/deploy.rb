f332702fdf09442af4944b4308a1646fdd11e2c3
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-07-11 18:00:28 +0900

    Revert "tmp: edit deploy setting2"
    This reverts commit b07ee2d01d9624efb552bf9ef0c8dae94dac0fb2.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,18 +9,14 @@ set :rbenv_ruby, '2.7.1'
 set :linked_files, %w{config/master.key .env}
 append :linked_dirs, "log", "public/system", "tmp"
 
-task :initial do
-  on roles(:app) do
-    before "deploy:restart", "puma:start"
-    invoke "deploy"
+namespace :deploy do
+  task :restart_puma do 
+    invoke "puma:stop"
+    invoke! "puma:start"
   end
 end
 
-task :restart do
-  on roles :app do
-    invoke "puma:restart"
-  end
-end
+after "deploy:finishing", "deploy:restart_puma"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
